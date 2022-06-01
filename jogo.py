9e814dbc8007dd1d9b0da1b115053cad14b8fd56
GiulianeEC <giuliane.correa@aluno.ufop.edu.br>
2022-06-01 14:12:23 -0300

    ovelhas andam

--- jogo.py
+++ jogo.py
@@ -1,14 +1,28 @@
+
+    #!/usr/bin/env python3
+# -*- codificacao: utf-8 -*-
+
+# !/usr/bin/env python3
 from math import inf as infinity
 from random import choice
 import platform
 import time
 from os import system
 
+"""
+Um versão simples do algoritmo MINIMAX para o Jogo da Velha.
+"""
+
+# Representando a variável que identifica cada jogador
+# HUMANO = Oponente humano
+# COMP = Agente Inteligente
+# tabuleiro = dicionário com os valores em cada posição (x,y)
+# indicando o jogador que movimentou nessa posição.
+# Começa vazio, com zero em todas posições.
 HUMANO = -1
 COMP = +1
 from random import randint
 lobo = ([0, 4])
-
 ovelha_1 = ([7, 1, 0])
 ovelha_2 = ([7, 3, 0])
 ovelha_3 = ([7, 5, 0])
@@ -25,8 +39,14 @@ tabuleiro = [
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 0, 1, 0, 1, 0, 1],
-
 ]
+
+"""
+Funcao para avaliacao heuristica do estado.
+:parametro (estado): o estado atual do tabuleiro
+:returna: +1 se o computador vence; -1 se o HUMANOo vence; 0 empate
+ """
+
 
 def avaliacao(estado):
     if vitoria_ovelha(estado, COMP):
@@ -38,9 +58,25 @@ def avaliacao(estado):
 
     return placar
 
+
+""" fim avaliacao (estado)------------------------------------- """
+
+
 def vitoria_lobo(estado, jogador):
+    """
+    Esta funcao testa se um jogador especifico vence. Possibilidades:
+    * Tres linhas     [X X X] or [O O O]
+    * Tres colunas    [X X X] or [O O O]
+    * Duas diagonais  [X X X] or [O O O]
+    :param. (estado): o estado atual do tabuleiro
+    :param. (jogador): um HUMANO ou um Computador
+    :return: True se jogador vence
+    """
     win_estado = [
-        [estado[7][1]], [estado[7][3]], [estado[7][5]], [estado[7][7]]      ]
+        [estado[7][1]], [estado[7][3]], [estado[7][5]], [estado[7][7]]  # toda linha 1
+    ]
+    # Se um, dentre todos os alinhamentos pertence um mesmo jogador, 
+    # então o jogador vence!
     if [jogador] in win_estado:
         return True
     else:
@@ -48,6 +84,21 @@ def vitoria_lobo(estado, jogador):
 
 
 def vitoria_ovelha(estado, jogador):
+    """
+    Esta funcao testa se um jogador especifico vence. Possibilidades:
+    * Tres linhas     [X X X] or [O O O]
+    * Tres colunas    [X X X] or [O O O]
+    * Duas diagonais  [X X X] or [O O O]
+    :param. (estado): o estado atual do tabuleiro
+    :param. (jogador): um HUMANO ou um Computador
+    :return: True se jogador vence
+    """
+    # win_estado = [
+    #     [estado[0][1]], [estado[0][3]], [estado[0][5]], [estado[0][7]] # toda linha 1
+    # ]
+    # Se um, dentre todos os alinhamentos pertence um mesmo jogador,
+    # então o jogador vence!
+
     cell = celulas_vazias_lobo(estado)
 
     if cell == []:
@@ -56,8 +107,25 @@ def vitoria_ovelha(estado, jogador):
         return False
 
 
+""" ---------------------------------------------------------- """
+
+"""
+Testa fim de jogo para ambos jogadores de acordo com estado atual
+return: será fim de jogo caso ocorra vitória de um dos jogadores.
+"""
+
+
 def fim_jogo(estado):
     return vitoria_lobo(estado, HUMANO) or vitoria_ovelha(estado, COMP)
+
+
+""" ---------------------------------------------------------- """
+
+"""
+Verifica celular vazias e insere na lista para informar posições
+ainda permitidas para próximas jogadas.
+"""
+
 
 def celulas_vazias(estado):
     celulas = []
@@ -90,17 +158,31 @@ def celulas_vaziasOvelha(estado):
         for y, cell in enumerate(row):
             if cell == 1:
 
-                if (x + 1) < 8 and (y - 1) >= 0 and estado[x + 1][y - 1] == 0:
-                    celulas.append([x + 1, y - 1])
+                if x - 1 >= 0 and y - 1 >= 0 and estado[x - 1][y - 1] == 0:
+                    celulas.append([x - 1, y - 1])
 
-                if x + 1 < 8 and y + 1 < 8 and estado[x + 1][y + 1] == 0:
-                    celulas.append([x + 1, y + 1])
+                if x - 1 >= 0 and y + 1 < 8 and estado[x - 1][y + 1] == 0:
+                    celulas.append([x - 1, y + 1])
 
     return celulas
 
 
+""" ---------------------------------------------------------- """
+
+"""
+Um movimento é valido se a célula escolhida está vazia.
+:param (x): coordenada X
+:param (y): coordenada Y
+:return: True se o tabuleiro[x][y] está vazio
+"""
+
+
 def movimento_valido(x, y):
+    # movimentos = {
+    #     1: [ovelha1[0] + 1, ovelha1[1] - 1], 2: [ovelha1[0] + 1, ovelha1[1] + 1],
+    # }
     if [x, y] in celulas_vaziasOvelha(tabuleiro):
+        # if [x, y] in movimentos:
         return True
     else:
         return False
@@ -114,23 +196,37 @@ def movimento_validoLobo(x, y):
         return False
 
 
+""" ---------------------------------------------------------- """
+
+"""
+Executa o movimento no tabuleiro se as coordenadas são válidas
+:param (x): coordenadas X
+:param (y): coordenadas Y
+:param (jogador): o jogador da vez
+"""
+
+
 def exec_movimento(x, y, jogador):
     if movimento_valido(x, y):
 
-        if (x == (ovelha_2[0] + 1) and y == (ovelha_2[1] + 1)) or (
-                x == (ovelha_2[0] + 1) and y == (ovelha_2[1] - 1)):
+        if (x == (ovelha_2[0] - 1) and y == (ovelha_2[1] + 1)) or (
+                x == (ovelha_2[0] - 1) and y == (ovelha_2[1] - 1)):
+            # ovelha_2[2] = 1
             ovelhas[1] = 1
 
-        if (x == (ovelha_1[0] + 1) and y == (ovelha_1[1] + 1)) or (
-                x == (ovelha_1[0] + 1) and y == (ovelha_1[1] - 1)):
+        if (x == (ovelha_1[0] - 1) and y == (ovelha_1[1] + 1)) or (
+                x == (ovelha_1[0] - 1) and y == (ovelha_1[1] - 1)):
+            #ovelha_1[2] = 1
             ovelhas[0] = 1
 
-        if (x == (ovelha_3[0] + 1) and y == (ovelha_3[1] + 1)) or (
-                x == (ovelha_3[0] + 1) and y == (ovelha_3[1] - 1)):
+        if (x == (ovelha_3[0] - 1) and y == (ovelha_3[1] + 1)) or (
+                x == (ovelha_3[0] - 1) and y == (ovelha_3[1] - 1)):
+         #  ovelha_3[2] = 1
            ovelhas[2] = 1
 
-        if (x == (ovelha_4[0] + 1) and y == (ovelha_4[1] + 1)) or (
-                x == (ovelha_4[0] + 1) and y == (ovelha_4[1] - 1)):
+        if (x == (ovelha_4[0] - 1) and y == (ovelha_4[1] + 1)) or (
+                x == (ovelha_4[0] - 1) and y == (ovelha_4[1] - 1)):
+           #  ovelha_4[2] = 1
             ovelhas[3] = 1
 
         ov = False
@@ -178,6 +274,7 @@ def exec_movimento(x, y, jogador):
             ovelha_4[2] = 0
 
         tabuleiro[x][y] = jogador
+        # tabuleiro[loboX][loboY] = 0
 
         return True
     else:
@@ -195,12 +292,26 @@ def exec_movimentoLobo(x, y, jogador):
         return False
 
 
+""" ---------------------------------------------------------- """
+
+"""
+Função da IA que escolhe o melhor movimento
+:param (estado): estado atual do tabuleiro
+:param (profundidade): índice do nó na árvore (0 <= profundidade <= 9),
+mas nunca será nove neste caso (veja a função iavez())
+:param (jogador): um HUMANO ou um Computador
+:return: uma lista com [melhor linha, melhor coluna, melhor placar]
+"""
+
+
 def minimax(estado, profundidade, jogador):
+    # valor-minmax(estado)
     if jogador == COMP:
         melhor = [-1, -1, -infinity]
     else:
         melhor = [-1, -1, +infinity]
 
+    # valor-minimax(estado) = avaliacao(estado)
     if profundidade == 0 or fim_jogo(estado):
         placar = avaliacao(estado)
         return [-1, -1, placar]
@@ -238,12 +349,27 @@ def minimax(estado, profundidade, jogador):
     return melhor
 
 
+""" ---------------------------------------------------------- """
+
+"""
+Limpa o console para SO Windows
+"""
+
+
 def limpa_console():
     os_name = platform.system().lower()
     if 'windows' in os_name:
         system('cls')
     else:
         system('clear')
+
+
+""" ---------------------------------------------------------- """
+
+"""
+Imprime o tabuleiro no console
+:param. (estado): estado atual do tabuleiro
+"""
 
 
 def exibe_tabuleiro(estado, comp_escolha, humano_escolha):
@@ -258,6 +384,18 @@ def exibe_tabuleiro(estado, comp_escolha, humano_escolha):
             else:
                 print('|', ' ', '|', end='')
     print('\n----------------------------------------')
+
+
+""" ---------------------------------------------------------- """
+
+"""
+Chama a função minimax se a profundidade < 9,
+ou escolhe uma coordenada aleatória.
+:param (comp_escolha): Computador escolhe X ou O
+:param (humano_escolha): HUMANO escolhe X ou O
+:return:
+"""
+
 
 def IA_vez(comp_escolha, humano_escolha):
     # profundidade = len(celulas_vazias(tabuleiro))
@@ -276,17 +414,32 @@ def IA_vez(comp_escolha, humano_escolha):
     time.sleep(1)
 
 
+""" ---------------------------------------------------------- """
+
+
 def HUMANO_vez(comp_escolha, humano_escolha):
+    """
+    O HUMANO joga escolhendo um movimento válido
+    :param comp_escolha: Computador escolhe X ou O
+    :param humano_escolha: HUMANO escolhe X ou O
+    :return:
+    """
     profundidade = 4
     if profundidade == 0 or fim_jogo(tabuleiro):
         return
 
+    # Dicionário de movimentos válidos
     movimento = -1
+    # movimentos = {
+    #     1: [0, 0], 2: [0, 1], 3: [0, 2],
+    #     4: [1, 0], 5: [1, 1], 6: [1, 2],
+    #     7: [2, 0], 8: [2, 1], 9: [2, 2],
+    # }
     movimentos = {
         3: [lobo[0] + 1, lobo[1] + 1], 1: [lobo[0] + 1, lobo[1] - 1], 7: [lobo[0] - 1, lobo[1] - 1],
         9: [lobo[0] - 1, lobo[1] + 1],
     }
-    # mudar movimentos
+
     limpa_console()
     print('Vez do HUMANO [{}]'.format(humano_escolha))
     exibe_tabuleiro(tabuleiro, comp_escolha, humano_escolha)
@@ -306,10 +459,11 @@ def HUMANO_vez(comp_escolha, humano_escolha):
             print('Tchau!')
             exit()
         except:
-            print('Escolha Inválida!')
+            print('Escolha Inválida! Escolha 1 ou 3 ou 7 ou 9')
+
 """ ---------------------------------------------------------- """
 """
-Funcao para testar teclas de opções de movimento do teclado
+Funcao para testar as teclas de opções de movimento do teclado
 """
 def testa_movimento(movimento):
 	if(movimento == 1 or movimento == 3 or movimento == 7 or movimento == 9):
@@ -319,13 +473,36 @@ def testa_movimento(movimento):
 
 """ ---------------------------------------------------------- """
 
+"""
+Funcao Principal que chama todas funcoes
+"""
+
 
 def main():
     limpa_console()
-    humano_escolha = 'L'  
-    comp_escolha = 'O'  
+    humano_escolha = 'L'  # Pode ser X ou O
+    comp_escolha = 'O'  # Pode ser X ou O
 
     primeiro = 'S'  # se HUMANO e o primeiro
+
+    # # HUMANO escolhe X ou O para jogar
+    # while humano_escolha != 'O' and humano_escolha != 'X':
+    #     try:
+    #         print('')
+    #         humano_escolha = input('Escolha X or O\n: ').upper()
+    #     except KeyboardInterrupt:
+    #         print('Tchau!')
+    #         exit()
+    #     except:
+    #         print('Escolha Errada')
+    #
+    # # Setting Computador's choice
+    # if humano_escolha == 'X':
+    #     comp_escolha = 'O'
+    # else:
+    #     comp_escolha = 'X'
+
+    # HUMANO pode começar primeiro
     limpa_console()
     while primeiro != 'S' and primeiro != 'N':
         try:
